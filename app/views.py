28dc3827c32a3b0b8124834fb25b6946cde50cf1
Ulya Harlamova <tellera111@gmail.com>
2019-02-02 18:35:32 +0300

    Add simple form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,4 +19,10 @@ def index():
         }
     ]
     return render_template("index.html", user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign In", form=form)
 
---
+++ config.py
@@ -0,0 +1,2 @@
+CSRF_ENABLED = True  # Prevent fake cross-site requests
+SECRET_KEY = 'this-is-sample-secret-key-which-protect-this-blog'
