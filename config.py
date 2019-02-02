daaf0baadc279487926af7752a5dead00d2ca7c2
Ulya Harlamova <tellera111@gmail.com>
2019-02-02 19:06:12 +0300

    Add providers, this commit isnt work correct

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 CSRF_ENABLED = True  # Prevent fake cross-site requests
 SECRET_KEY = 'this-is-sample-secret-key-which-protect-this-blog'
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
