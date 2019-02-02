b2becb2f4d32c50dd6f392643cece3257a3d9857
Ulya Harlamova <tellera111@gmail.com>
2019-02-02 18:48:15 +0300

    Some improved work of form

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"nickname":"ulya"}
+    user = {"nickname": "ulya"}
     posts = [  # список выдуманных постов
         {
             'author': {'nickname': 'John'},
@@ -24,5 +24,8 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID = "' + \
+              form.openid.data + '", remember me = ' + str(form.remember_me.data))
+        return redirect("/index")
     return render_template('login.html', title="Sign In", form=form)
-
