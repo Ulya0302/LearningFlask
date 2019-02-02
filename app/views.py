e4c9403c059e26daf69dcb73aa16d702efcdfbde
Ulya Harlamova <tellera111@gmail.com>
2019-02-02 18:09:04 +0300

    Add simple templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {"nickname":"ulya"}
+    posts = [  # список выдуманных постов
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", user=user, posts=posts)
 
