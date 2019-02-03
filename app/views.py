0ec5220998406c7352185510dff821d376202e4d
Ulya Harlamova <tellera111@gmail.com>
2019-02-03 10:43:24 +0300

    Change url for more flexibility

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 from app import app
 from app.forms import LoginForm
@@ -27,7 +27,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID = "' + \
               form.login.data + '", remember me = ' + str(form.remember_me.data))
-        return redirect("/index")
+        return redirect(url_for('index'))
     return render_template('login.html',
                            title="Sign In",
                            form=form)
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    CSRF_ENABLED = True  # Prevent fake cross-site requests
+    # CSRF_ENABLED = True  # Prevent fake cross-site requests
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
                  'this-is-sample-secret-key-which-protect-this-blog'
