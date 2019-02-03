e5590997e5647b04009f47c1006fa9cc9291e870
Ulya Harlamova <tellera111@gmail.com>
2019-02-03 10:36:09 +0300

    Change config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-CSRF_ENABLED = True  # Prevent fake cross-site requests
-SECRET_KEY = 'this-is-sample-secret-key-which-protect-this-blog'
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
-]
+import os
+
+
+class Config(object):
+    CSRF_ENABLED = True  # Prevent fake cross-site requests
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+                 'this-is-sample-secret-key-which-protect-this-blog'
